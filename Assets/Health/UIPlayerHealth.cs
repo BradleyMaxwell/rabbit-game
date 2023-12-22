ce1d13914fea07f856b6dbdefd0763a2ba9ebb79
BradleyMaxwell <72202351+BradleyMaxwell@users.noreply.github.com>
2023-12-22 17:44:07 +0000

    player UI health positioned
    The hearts that represent the player's health is now positioned in the bottom left of the screen when they are created on start.

--- Assets/Health/UIPlayerHealth.cs
+++ Assets/Health/UIPlayerHealth.cs
@@ -1,8 +1,5 @@
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
-using UnityEngine.Assertions.Must;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(GameEventListener))] // need to listen for the player's health changing
@@ -26,22 +23,30 @@ public class UIPlayerHealth : MonoBehaviour // changed how the health hearts are
         int healthChange = playerHealthInfo.CurrentHealth - CurrentHealthDisplayed; // find how much the hearts need to change
         if (healthChange < 0) // the player's health has gone down
         {
-            // go left changing hearts to missing hearts
+            RemoveHearts();
+        } else if (healthChange > 0) // the player's health has gone up
+        {
+            AddHearts();
+        }
+        CurrentHealthDisplayed = playerHealthInfo.CurrentHealth; // current health displayed needs updating for when this function is next triggered by the health change event
+    }
+
+    private void RemoveHearts() // go left changing hearts to missing hearts
+    {
             for (int i = currentHealthDisplayed - 1; i > playerHealthInfo.CurrentHealth - 1; i--)
             {
                 Image currentHeartImage = hearts[i].GetComponent<Image>();
                 currentHeartImage.color = missingHeartImage.color;
             }
-        } else if (healthChange > 0) // the player's health has gone up
-        {
-            // go right changing hearts from missing hearts to hearts, starting from the next heart in the list as that is the first missing heart
-            for (int i = currentHealthDisplayed; i <= playerHealthInfo.CurrentHealth - 1; i++)
+    }
+
+    private void AddHearts() // go right changing hearts from missing hearts to hearts, starting from the next heart in the list as that is the first missing heart
+    {
+        for (int i = currentHealthDisplayed; i <= playerHealthInfo.CurrentHealth - 1; i++)
             {
                 Image currentMissingHeartImage = hearts[i].GetComponent<Image>();
                 currentMissingHeartImage.color = heartImage.color;
             }
-        }
-        CurrentHealthDisplayed = playerHealthInfo.CurrentHealth; // current health displayed needs updating for when this function is next triggered by the health change event
     }
 
     private void SetupHearts() // called when the scene starts to setup the hearts and position them correctly based on the player's max health
@@ -50,7 +55,7 @@ public class UIPlayerHealth : MonoBehaviour // changed how the health hearts are
         {
             GameObject heart = Instantiate(heartPrefab, transform); // create a heart prefab as a child element of this parent game object
             hearts.Add(heart);
-            float offset = i * heart.GetComponent<RectTransform>().rect.width; // how much to move the current heart UI element to the right by so each heart is shown to the right of the previous
+            float offset = i * GetComponent<RectTransform>().rect.width; // how much to move the current heart UI element to the right by so each heart is shown to the right of the previous
             heart.transform.localPosition = new Vector3(offset, 0f, 0f);
         }
         CurrentHealthDisplayed = playerHealthInfo.CurrentHealth;
